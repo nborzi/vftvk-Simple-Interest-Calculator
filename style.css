c06f36cbd51648677e6f259dc21be33b485fe743
nborzi <86881008+nborzi@users.noreply.github.com>
2021-07-05 16:12:09 +0100

    Add files via upload

---
+++ style.css
@@ -0,0 +1,25 @@
+.maindiv {
+    background-color:white;
+    width:300px;
+    padding:20px;
+    border-radius:25px;
+    :center;
+    color:black;
+}
+
+
+.highlight
+{
+    background-color: yellow;
+}
+
+body
+
+{background-color:black;font-family:arial;color:white}
+
+h1{color:grey;font-family:verdana}
+
+
+
+
+
